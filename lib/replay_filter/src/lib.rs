dd6caa3c0a9ec590833192669f4fa7a39d3dedf4
nullchinchilla <nullchinchilla@pm.me>
2023-03-06 12:24:56 -0700

    fix

--- lib/replay_filter/src/lib.rs
+++ lib/replay_filter/src/lib.rs
@@ -1,31 +1,38 @@
-use std::collections::BTreeSet;
+use bitvec::prelude::BitArray;
 
 /// A filter for replays. Records recently seen seqnos and rejects either repeats or really old seqnos.
 #[derive(Debug, Default)]
 pub struct ReplayFilter {
-    top_seqno: u64,
     bottom_seqno: u64,
-    seen_seqno: BTreeSet<u64>,
+    bitmap: BitArray<[usize; 1024]>,
 }
 
 impl ReplayFilter {
+    /// Adds a new sequence number to the replay filter. Returns whether this is accepted.
     pub fn add(&mut self, seqno: u64) -> bool {
-        if seqno < self.bottom_seqno {
-            // out of range. we can't know, so we just say no
-            return false;
+        loop {
+            if seqno < self.bottom_seqno {
+                // out of range. we can't know, so we just say no
+                return false;
+            }
+            let position = (seqno - self.bottom_seqno) as usize;
+            if let Some(mut posn) = self.bitmap.get_mut(position) {
+                if posn.replace(true) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            // we are out of range in the "future" side. we thus frame-shift the whole thing.
+            self.frame_shift((seqno + self.bottom_seqno) / 2);
         }
-        // check the seen
-        if self.seen_seqno.contains(&seqno) {
-            return false;
-        }
-        self.seen_seqno.insert(seqno);
-        self.top_seqno = seqno.max(self.top_seqno);
-        self.bottom_seqno = self.top_seqno.saturating_sub(10000);
-        self.seen_seqno = self.seen_seqno.split_off(&self.bottom_seqno);
-        // while self.top_seqno - self.bottom_seqno > 10000 {
-        //     self.seen_seqno.remove(&self.bottom_seqno);
-        //     self.bottom_seqno += 1;
-        // }
-        true
+    }
+
+    fn frame_shift(&mut self, new_starting: u64) {
+        let shift_amount = (new_starting - self.bottom_seqno) as usize;
+        log::debug!("frameshift {shift_amount}");
+        let limit = self.bitmap.len() - 1;
+        self.bitmap.shift_left(shift_amount.min(limit));
+        self.bottom_seqno = new_starting;
     }
 }
