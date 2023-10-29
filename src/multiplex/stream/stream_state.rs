cb5a6305ece0d79362532771f8e4fbf90aa3938a
nullchinchilla <nullchinchilla@pm.me>
2023-10-28 20:17:18 -0400

    Refactor stream state condition check by removing the recovery check

--- src/multiplex/stream/stream_state.rs
+++ src/multiplex/stream/stream_state.rs
@@ -368,9 +368,8 @@ impl StreamState {
 
             // okay, we don't have retransmissions. this means we get to send a "normal" packet.
             let mut queues = self.queues.lock();
-            if self.inflight.inflight() < MAX_CWND
-                && !queues.write_stream.is_empty()
-                && !self.in_recovery
+            if self.inflight.inflight() < MAX_CWND && !queues.write_stream.is_empty()
+            // && !self.in_recovery
             {
                 log::debug!("send window has grown to {}", self.inflight.inflight());
                 let mut buffer = vec![0; MSS];
