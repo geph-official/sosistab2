b7ab100879f247c687e59906bb40a655b7f22c39
nullchinchilla <nullchinchilla@pm.me>
2023-11-08 18:45:57 -0500

    Add timestamp to trace logs for outgoing and incoming messages in multiplex module

--- src/multiplex/trace.rs
+++ src/multiplex/trace.rs
@@ -1,15 +1,17 @@
 use once_cell::sync::Lazy;
-use std::fs::File;
 use std::io::Write;
+use std::{fs::File, time::Instant};
 
 use super::pipe_pool::Message;
+
+static START: Lazy<Instant> = Lazy::new(Instant::now);
 
 pub fn trace_outgoing_msg(msg: &Message) {
     static TRACE_OUTGOING: Lazy<Option<File>> = Lazy::new(|| {
         if let Ok(fname) = std::env::var("SOSISTAB_TRACE_OUTGOING") {
             let mut file =
                 File::create(&fname).expect("cannot create file for SOSISTAB_TRACE_OUTGOING");
-            writeln!(file, "kind,stream_id,seqno,payload_len").unwrap();
+            writeln!(file, "time,kind,stream_id,seqno,payload_len").unwrap();
             Some(File::create(&fname).expect("cannot create file for SOSISTAB_TRACE_OUTGOING"))
         } else {
             None
@@ -24,7 +26,13 @@ pub fn trace_outgoing_msg(msg: &Message) {
             payload,
         } = msg
         {
-            let _ = writeln!(inner, "{:?},{stream_id},{seqno},{}", kind, payload.len());
+            let _ = writeln!(
+                inner,
+                "{},{:?},{stream_id},{seqno},{}",
+                START.elapsed().as_secs_f64() * 1000.0,
+                kind,
+                payload.len()
+            );
         }
     }
 }
@@ -34,7 +42,7 @@ pub fn trace_incoming_msg(msg: &Message) {
         if let Ok(fname) = std::env::var("SOSISTAB_TRACE_INCOMING") {
             let mut file =
                 File::create(&fname).expect("cannot create file for SOSISTAB_TRACE_INCOMING");
-            writeln!(file, "kind,stream_id,seqno,payload_len").unwrap();
+            writeln!(file, "time,kind,stream_id,seqno,payload_len").unwrap();
             Some(File::create(&fname).expect("cannot create file for SOSISTAB_TRACE_INCOMING"))
         } else {
             None
@@ -49,7 +57,13 @@ pub fn trace_incoming_msg(msg: &Message) {
             payload,
         } = msg
         {
-            let _ = writeln!(inner, "{:?},{stream_id},{seqno},{}", kind, payload.len());
+            let _ = writeln!(
+                inner,
+                "{},{:?},{stream_id},{seqno},{}",
+                START.elapsed().as_secs_f64() * 1000.0,
+                kind,
+                payload.len()
+            );
         }
     }
 }
