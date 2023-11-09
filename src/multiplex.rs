190fa105dd4d07c7f5a2ac17a20d1ba370b7eafa
nullchinchilla <nullchinchilla@pm.me>
2023-11-08 21:03:12 -0500

    Update timer delay from 5 seconds to 5 milliseconds in multiplex module

--- src/multiplex.rs
+++ src/multiplex.rs
@@ -190,7 +190,7 @@ async fn tick_loop(
         }
         // sleep first to prevent too aggressively looping around
         // this is also the basis for the brand of delayed-ack handling we do
-        timer.set_at(Instant::now() + Duration::from_secs(5));
+        timer.set_at(Instant::now() + Duration::from_millis(5));
         (&mut timer).await;
         timer.set_at(next_tick);
         // horrifying hax
