e8b6de8c4e43396b54ac56b4d99b210e5dd72413
nullchinchilla <nullchinchilla@pm.me>
2023-11-08 20:43:48 -0500

    Increase MSS constant and switch from HSTCP to BIC congestion control in StreamState

--- src/multiplex/stream/stream_state.rs
+++ src/multiplex/stream/stream_state.rs
@@ -18,7 +18,7 @@ use crate::{
 };
 
 use super::{inflight::Inflight, StreamQueues};
-const MSS: usize = 1000;
+const MSS: usize = 10000;
 
 pub struct StreamState {
     phase: Phase,
@@ -245,18 +245,18 @@ impl StreamState {
                     }
 
                     // use BIC congestion control
-                    // let bic_inc = if self.cwnd < self.cwnd_max {
-                    //     (self.cwnd_max - self.cwnd) / 2.0
-                    // } else {
-                    //     self.cwnd - self.cwnd_max
-                    // }
-                    // .max(n as f64 * 0.5);
-                    // log::trace!("bic_inc = {bic_inc}");
-                    // self.cwnd += bic_inc / self.cwnd;
+                    let bic_inc = if self.cwnd < self.cwnd_max {
+                        (self.cwnd_max - self.cwnd) / 2.0
+                    } else {
+                        self.cwnd - self.cwnd_max
+                    }
+                    .max(n as f64 * 0.5);
+                    log::trace!("bic_inc = {bic_inc}");
+                    self.cwnd += bic_inc / self.cwnd;
 
                     // use HSTCP
-                    let incr = self.cwnd.powf(0.4).max(1.0);
-                    self.cwnd += incr / self.cwnd;
+                    // let incr = self.cwnd.powf(0.4).max(1.0);
+                    // self.cwnd += incr / self.cwnd;
 
                     log::trace!("{n} acks received, increasing cwnd to {:.2}", self.cwnd);
                     log::debug!(
@@ -332,8 +332,8 @@ impl StreamState {
             // self.cwnd = self.cwnd.max(1.0);
 
             // HSTCP
-            let factor = 0.75;
-            self.cwnd *= factor;
+            // let factor = 0.75;
+            // self.cwnd *= factor;
             // self.cwnd = self.cwnd.max(self.inflight.bdp() as f64 * factor).max(1.0);
 
             self.global_cwnd_guess
