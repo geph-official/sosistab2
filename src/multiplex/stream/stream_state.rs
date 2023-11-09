42738af43bd9b371de5ae058e4935dc38520e8e3
nullchinchilla <nullchinchilla@pm.me>
2023-11-08 20:47:42 -0500

    Re-enable stream state congestion window adjustments

--- src/multiplex/stream/stream_state.rs
+++ src/multiplex/stream/stream_state.rs
@@ -327,9 +327,9 @@ impl StreamState {
             } else {
                 self.cwnd_max = self.cwnd;
             }
-            // self.cwnd_max = self.cwnd_max.max(self.inflight.bdp() as f64);
-            // self.cwnd *= 1.0 - beta;
-            // self.cwnd = self.cwnd.max(1.0);
+            self.cwnd_max = self.cwnd_max.max(self.inflight.bdp() as f64);
+            self.cwnd *= 1.0 - beta;
+            self.cwnd = self.cwnd.max(1.0);
 
             // HSTCP
             // let factor = 0.75;
