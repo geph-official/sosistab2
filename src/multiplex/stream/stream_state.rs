920cc6f288451887201b74fc37f953f207251367
nullchinchilla <nullchinchilla@pm.me>
2023-11-09 19:26:56 -0500

    Set minimum value for calculated speed to 50.0 in StreamState

--- src/multiplex/stream/stream_state.rs
+++ src/multiplex/stream/stream_state.rs
@@ -377,7 +377,7 @@ impl StreamState {
         }
 
         // hardcoded speed limit
-        let speed = self.cwnd / self.inflight.min_rtt().as_secs_f64();
+        let speed = (self.cwnd / self.inflight.min_rtt().as_secs_f64()).max(50.0);
         let mut writes_allowed = (now
             .saturating_duration_since(self.last_write_time)
             .as_secs_f64()
